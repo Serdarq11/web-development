152ad2e336d29a9b3c8870e07e92650a16426e53
Serdarq11 <serdarkaya.q@gmail.com>
2025-07-21 14:04:03 +0300

    added logic conditionals and truthy_falsy

---
+++ 16-JS-Decision-Making/Logical_Operators/app.js
@@ -0,0 +1,24 @@
+const password = prompt("Enter your password: ");
+
+if(password.length >= 6 && password.indexOf(" ") === -1){
+    console.log("Valid Password");
+} else {
+    console.log("Incorrect format for password");
+}
+
+const age = -1;
+
+if(age >= 0 && age < 5 || age >= 65){
+    console.log("FREE");
+} else if(age >= 5 && age < 10) {
+    console.log("$10");
+} else if(age >= 10 && age < 65){
+    console.log("$20");
+} else{
+    console.log("INVALID AGE!");
+}
+
+let firstName = prompt("enter your first name");
+if(!firstName){
+    firstname = prompt("try again!");
+}
