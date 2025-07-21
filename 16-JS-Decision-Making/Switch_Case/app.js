459c4709857221deeb72838a4e32a12e4399ec17
Serdarq11 <serdarkaya.q@gmail.com>
2025-07-21 15:52:39 +0300

    added switch case

---
+++ 16-JS-Decision-Making/Switch_Case/app.js
@@ -0,0 +1,42 @@
+const day = 4;
+
+// if(day === 1){
+//     console.log("monday");
+// }
+// else if(day === 2){
+//     console.log("tuesday");
+// }
+// else if(day === 3){
+//     console.log("wednesday");
+// }
+// else if(day === 4){
+//     console.log("thursday");
+// }
+// else if(day === 5){
+//     console.log("friday");
+// }
+// else{
+//     console.log("I don't know that!");
+// }
+
+// switch solution
+
+switch(day){
+    case 1:
+        console.log("monday");
+        break;
+    case 2:
+        console.log("tuesday");
+        break;
+    case 3:
+        console.log("wednesday");
+        break;
+    case 4:
+        console.log("thursday");
+        break;
+    case 5:
+        console.log("friday");
+        break;
+    default:
+        console.log("I don't know that!");
+}
