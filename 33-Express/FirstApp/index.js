83ef3afb9ccdaff444ea6099583e299197ac7e4e
Serdarq11 <serdarkaya.q@gmail.com>
2025-08-22 19:08:30 +0300

    added content of express to the FirstApp file

--- 33-Express/FirstApp/index.js
+++ 33-Express/FirstApp/index.js
@@ -10,12 +10,21 @@ const app = express();      // it is a method
 //     res.send('<h1>This is my WebPagepage!</h1>'); // sending html tag       -> This is my Webpage! -> can be seen in the page localhost:8080
 // })
 
-// app.get('*', (req, res) => {
-//     res.send(`I don't know that path!!!`);   // it give me an error check this later!!!
-// });
+
 
 app.get('/', (req,res) => {
-    res.send('This is the HomePage');   // 'This is the HomePage' on the page -> localhost:8080
+    res.send('Welcome is the HomePage');   // 'This is the HomePage' on the page -> localhost:8080
+})
+
+app.get('/r/:subreddit', (req,res) => {
+    const { subreddit } = req.params;
+    res.send(`<h1> Browsing the ${subreddit} subreddit!!! </h1>`);     // Browsing the backyardchickens subreddit!!! -> localhost:8080 , when typing localhost:8080/r/backyardchickens
+})
+
+
+app.get('/r/:subreddit/:postId', (req,res) => {
+    const { subreddit, postId } = req.params;
+    res.send(`<h1> Viewing post id ${postId} on the ${subreddit} subreddit!!! </h1>`);     // Viewing post id 23728 on the gardening subreddit!!! -> localhost:8080 , when typing localhost:8080/r/gardening/23728
 })
 
 app.get('/cats', (req,res) => {
@@ -30,6 +39,20 @@ app.post('/cats', (req,res) => {
 
 app.get('/dogs', (req,res) => {
     res.send('WOOF');   // 'WOOF' on the page -> localhost:8080/dogs
+})
+
+
+app.get('/search', (req,res) => {
+    const { q } = req.query;
+    if(!q){
+        res.send('NOTHING FOUND IF NOTHING SEARCHED!')
+    }
+    res.send(`<h1> SEARCH RESULTS FOR: ${q}! </h1>`); // SEARCH RESULTS FOR: dogs! -> localhost:8080/search?q=dogs
+})
+
+
+app.get(/(.*)/, (req, res) => {
+    res.send(`I don't know that path.`);    // anything else except /cats, /dogs, and /
 })
 
 
