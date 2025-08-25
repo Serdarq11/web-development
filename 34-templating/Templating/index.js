e7df00634b9ad6f0c5b292e7aaa788d1ecdfbc9e
Serdarq11 <serdarkaya.q@gmail.com>
2025-08-25 21:23:35 +0300

    added data.json

--- 34-templating/Templating/index.js
+++ 34-templating/Templating/index.js
@@ -1,12 +1,39 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const redditData = require('./data.json');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));   // used for reaching index.js from anywhere we want in bash using /.../etc.
 
 app.get('/' , (req,res) => {
     res.render('home');     // content of home.ejs seen in localhost:3030
+})
+
+app.get('/cats', (req,res) => {
+    const cats = [
+        'Blue', 'Rocket', 'Monty', 'Stephanie', 'Winston'
+    ]
+    res.render('cats', { cats })
+})
+
+app.get('/r/:subreddit', (req,res) => {
+    const { subreddit } = req.params;
+    const data = redditData[subreddit];
+    console.log(data);
+
+    if(data){
+        res.render('subreddit', { ...data });
+    } else{
+        res.render('notfound', { subreddit });
+    }
+})
+
+app.get('/rand' , (req,res) => {
+    const num = Math.floor(Math.random() * 10) + 1;
+    res.render('random', { num });     // content of render.ejs seen in localhost:3030
 })
 
 app.listen(3000, () => {
