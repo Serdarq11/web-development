b6df0c6cd8f1cfebb3ef1b539e8e7a8511ed4820
Serdarq11 <serdarkaya.q@gmail.com>
2025-08-28 15:03:32 +0300

    added edit and delete (delete method is in show.ejs)

--- 35-RESTful-Routes/REST_Demo/index.js
+++ 35-RESTful-Routes/REST_Demo/index.js
@@ -1,46 +1,48 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const methodOverride = require('method-override');
 const { v4: uuidv4 } = require('uuid');
 uuidv4();
 
-app.use(express.urlencoded({ extended : true }))    // for parsing req.body
+app.use(express.urlencoded({ extended: true }))    // for parsing req.body
 app.use(express.json());
+app.use(methodOverride('_method'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-const comments = [
+let comments = [
     {
         id: uuidv4(),
-        username : 'Todd',
-        comment : 'lol that is so funny!'
+        username: 'Todd',
+        comment: 'lol that is so funny!'
     },
     {
         id: uuidv4(),
-        username : 'Skyler',
-        comment : 'I like to go birdwatching with my dog!'
+        username: 'Skyler',
+        comment: 'I like to go birdwatching with my dog!'
     },
     {
         id: uuidv4(),
-        username : 'Sk8erBoi',
-        comment : 'Plz delete your account Todd!'
+        username: 'Sk8erBoi',
+        comment: 'Plz delete your account Todd!'
     },
     {
         id: uuidv4(),
-        username : 'onlysayswoof',
-        comment : 'woof woof woof!'
+        username: 'onlysayswoof',
+        comment: 'woof woof woof!'
     },
 ]
 
-app.get('/comments', (req,res) => {
+app.get('/comments', (req, res) => {
     res.render('comments/index', { comments });     // read comments
 })
 
-app.get('/comments/new', (req,res) => {
+app.get('/comments/new', (req, res) => {
     res.render('comments/new')
 })
 
-app.post('/comments', (req,res) => {            // adding data
+app.post('/comments', (req, res) => {            // adding data
     //console.log(req.body);  //shown in bash
     const { username, comment } = req.body;
     comments.push({ username, comment, id: uuidv4() });   // added username and comment to the list of comments
@@ -48,13 +50,19 @@ app.post('/comments', (req,res) => {            // adding data
     res.redirect('/comments');  // if we don't do that, there will be resubmit the form alert in the page.
 })
 
-app.get('/comments/:id', (req,res) => {
+app.get('/comments/:id', (req, res) => {
     const { id } = req.params;
     const comment = comments.find(c => c.id === id);
     res.render('comments/show', { comment });
 })
 
-app.patch('/comments/:id', (req,res) => {
+app.get('/comments/:id/edit', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === id);
+    res.render('comments/edit', { comment })
+})
+
+app.patch('/comments/:id', (req, res) => {
     const { id } = req.params;
     const newCommentText = req.body.comment
     const foundComment = comments.find(c => c.id === id);
@@ -63,11 +71,17 @@ app.patch('/comments/:id', (req,res) => {
     // res.send('UPDATING SOMETHING');
 })
 
-app.get('/tacos', (req,res) => {
+app.delete('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    comments = comments.filter(c => c.id !== id);
+    res.redirect('/comments')
+})
+
+app.get('/tacos', (req, res) => {
     res.send('GET /tacos response')
 })
 
-app.post('/tacos', (req,res) => {
+app.post('/tacos', (req, res) => {
     // console.log(req.body);      // by default, it is undefined  { meat: 'dad', qty: '3' } after app.use(express.urlencoded({ extended : true }))
     const { meat, qty } = req.body; // we type the value of meat and qty to the postman body section (key value pairs)
     res.send(`OK, here are your ${qty} ${meat} tacos.`)
